4e883d94794f28501a77abba21bf827f060dcf21
Anh-Ton <atru0019@student.monash.edu>
2025-05-09 12:02:28 +1000

    Added different scenarios

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static final int SIMULATION_ITERATIONS_PER_RATIO = 500;
@@ -8,37 +9,113 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean runWithVisals = true;
+        // -------------------------- //
+        boolean runWithVisals = false;
+        // -------------------------- //
+
+        ArrayList<int[][]> expressConfigurations = new ArrayList<>();
+        ArrayList<String> configNames = new ArrayList<>();
 
         int[][] noExpress = {
-                {0},
-                {0},
-                {0},
-                {0},
-                {0},
-                {0},
-                {3}
+                {},
+                {},
+                {},
+                {},
+                {},
+                {},
+                {}
         };
+        expressConfigurations.add(noExpress);
+        configNames.add("noExpress");
+
+        int[][] oneExpress = {
+                {},
+                {},
+                {},
+                {},
+                {},
+                {},
+                {1,2,3,4,5,6,7,8}
+        };
+        expressConfigurations.add(oneExpress);
+        configNames.add("oneExpress");
+
+        int[][] twoExpress = {
+                {},
+                {},
+                {},
+                {},
+                {},
+                {1,2,3,4,5, 9,10,11},
+                {1,2,3,4,5,6,7,8}
+        };
+        expressConfigurations.add(twoExpress);
+        configNames.add("twoExpress");
+
+        int[][] twoExpressV2 = {
+                {},
+                {},
+                {},
+                {},
+                {},
+                {1,2,3,4,5,6,7,8},
+                {1,2,3,4,5,6,7,8}
+        };
+        expressConfigurations.add(twoExpressV2);
+        configNames.add("twoExpressV2");
+
+        int[][] threeExpress = {
+                {},
+                {},
+                {},
+                {},
+                {1,2, 6,7,8,9,10,11},
+                {1,2,3,4,5, 9,10,11},
+                {1,2,3,4,5,6,7,8}
+        };
+        expressConfigurations.add(threeExpress);
+        configNames.add("threeExpress");
+
+        int[][] threeExpressV2 = {
+                {},
+                {},
+                {},
+                {},
+                {1,2,3,4,5,6,7,8},
+                {1,2,3,4,5,6,7,8},
+                {1,2,3,4,5,6,7,8}
+        };
+        expressConfigurations.add(threeExpressV2);
+        configNames.add("threeExpressV2");
 
         if (runWithVisals) {
-            for (int i = 0; i < 500; i++) {
-                Building menziesBuilding = new Building(noExpress);
-                menziesBuilding.runWithVisuals();
+            for (int[][] expressConfiguration : expressConfigurations) {
+                for (int i = 0; i < 100; i++) {
+                    Building menziesBuilding = new Building(expressConfiguration);
+                    menziesBuilding.runWithVisuals();
+                }
             }
         }
 
         else{
             try {
                 BufferedWriter writer = new BufferedWriter(new FileWriter("simulation_results.csv"));
-                for (int i = 0; i < SIMULATION_ITERATIONS_PER_RATIO; i++) {
-                    if (i % MAX_CELL_ROW_LENGTH == 0) {
-                        writer.write("\n");
+                for (int j = 0; j < expressConfigurations.size(); j++) {
+
+                    writer.write(configNames.get(j));
+                    for (int i = 0; i < SIMULATION_ITERATIONS_PER_RATIO; i++) {
+                        if (i % MAX_CELL_ROW_LENGTH == 0) {
+                            writer.write("\n");
+                        }
+
+                        Building menziesBuilding = new Building(expressConfigurations.get(j));
+                        menziesBuilding.run();
+                        writer.write(menziesBuilding.secondElapsed + ",");
                     }
 
-                    Building menziesBuilding = new Building(noExpress);
-                    menziesBuilding.run();
-                    writer.write(menziesBuilding.secondElapsed + ",");
+                    writer.write("\n\n\n");
                 }
+
                 writer.close();
 
             } catch (IOException e) {
